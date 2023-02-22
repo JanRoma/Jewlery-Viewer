ccfaf1305f194d484e22af7479321df013645063
Jan Romanowski <jan.romanowski2@gmail.com>
2023-02-22 15:27:45 +0100

    Basic room with the object and top light

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,27 +1,17 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
-const scene = new THREE.Scene()
+const scene : THREE.Scene = new THREE.Scene()
 
-const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-)
-camera.position.z = 2
+const camera = createPerspectiveCamera()
 
-const renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
+const renderer = createRenderer()
 document.body.appendChild(renderer.domElement)
 
-const geometry = new THREE.BoxGeometry()
-const material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00,
-    wireframe: true,
-})
-
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
+addOrbitControls(camera, renderer)
+addRoomEnvironment(scene)
+addMainObject(scene)
+addTopLight(scene)
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
@@ -34,14 +24,70 @@ function onWindowResize() {
 function animate() {
     requestAnimationFrame(animate)
 
-    cube.rotation.x += 0.01
-    cube.rotation.y += 0.01
 
     render()
 }
 
 function render() {
     renderer.render(scene, camera)
-}``
+}
+
+function addRoomEnvironment(scene: THREE.Scene) {
+    const roomGeometry = new THREE.BoxGeometry(2,2,2)
+    const roomMaterial = new THREE.MeshLambertMaterial( {color: 0xaaaaaa, side: THREE.BackSide} )
+    const room = new THREE.Mesh(roomGeometry, roomMaterial)
+    room.castShadow = true
+    room.receiveShadow = true
+    scene.add(room)
+}
+
+function addOrbitControls(camera : THREE.Camera, renderer : THREE.Renderer ){
+    const controls = new OrbitControls(camera, renderer.domElement)
+}
+
+function createRenderer() : THREE.Renderer {
+    const renderer : THREE.WebGLRenderer = new THREE.WebGLRenderer()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    renderer.shadowMap.enabled = true
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap
+
+    return renderer
+}
 
 animate()
+function addMainObject(scene: THREE.Scene) {
+    const geometry = new THREE.BoxGeometry(0.5,0.5,0.5)
+    const material = new THREE.MeshLambertMaterial({
+        color: 0x00ff00,
+        wireframe: false,
+    })
+
+    const mainObject = new THREE.Mesh(geometry, material)
+    mainObject.castShadow = true
+    mainObject.receiveShadow = true
+    scene.add(mainObject)
+}
+
+function createPerspectiveCamera() : THREE.PerspectiveCamera {
+    const camera : THREE.PerspectiveCamera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+    )
+    camera.position.z = 2
+    return camera
+}
+
+function addTopLight(scene: THREE.Scene) {
+    const light = new THREE.SpotLight()
+    light.castShadow = true;
+    light.shadow.mapSize.width = 512;
+    light.shadow.mapSize.height = 512;
+
+    const helper = new THREE.SpotLightHelper(light)
+
+    scene.add(light)
+    scene.add(helper)
+}
+
